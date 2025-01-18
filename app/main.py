8d93d0efd1a251b51c4b3ff1bf9f373d59bb8c71
Juan Ignacio Sivori <75853886+JuaniSivo@users.noreply.github.com>
2025-01-18 16:34:41 +0100

    test

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 # main.py
 
 import streamlit as st
-from lasio import LASFile
-from app.preprocessing import clean_data
-from app.visualization import plot_logs
-from app.clustering import apply_kmeans
+# import lasio
+# from app.preprocessing import clean_data
+# from app.visualization import plot_logs
+# from app.clustering import apply_kmeans
 
 def load_file():
     """Loads a .las file and returns a LASFile object."""
@@ -26,26 +26,26 @@ def app():
     st.title("Well Logs Analysis")
     st.write("Welcome to the interactive well logs analysis tool.")
 
-    las = load_file()
+    # las = load_file()
 
-    if las:
-        display_metadata(las)
+    # if las:
+    #     display_metadata(las)
 
-        if st.checkbox("Show Curve Data"):
-            st.dataframe(las.df())
+    #     if st.checkbox("Show Curve Data"):
+    #         st.dataframe(las.df())
 
-        selected_curves = st.multiselect(
-            "Select curves to plot:",
-            options=[curve.mnemonic for curve in las.curves]
-        )
+    #     selected_curves = st.multiselect(
+    #         "Select curves to plot:",
+    #         options=[curve.mnemonic for curve in las.curves]
+    #     )
 
-        if selected_curves:
-            plot_logs(las.df(), selected_curves)
+    #     if selected_curves:
+    #         plot_logs(las.df(), selected_curves)
 
-        if st.checkbox("Apply Clustering"):
-            n_clusters = st.slider("Number of clusters:", min_value=2, max_value=10, value=3)
-            clustered_data = apply_kmeans(las.df(), selected_curves, n_clusters)
-            st.dataframe(clustered_data)
+    #     if st.checkbox("Apply Clustering"):
+    #         n_clusters = st.slider("Number of clusters:", min_value=2, max_value=10, value=3)
+    #         clustered_data = apply_kmeans(las.df(), selected_curves, n_clusters)
+    #         st.dataframe(clustered_data)
 
 if __name__ == "__main__":
     app()
