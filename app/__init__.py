945ec04994026146cfff266b3ccf10a3a67b1316
Juan Ignacio Sivori <75853886+JuaniSivo@users.noreply.github.com>
2025-01-18 16:13:24 +0100

    main and init files to test the streamlit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# __init__.py
+
+# This file allows the "app" directory to be treated as a module in Python.
+
+# Import the main modules of the app package
+default_imports = [
+    "main",
+    "preprocessing",
+    "visualization",
+    "clustering",
+    "utils"
+]
+
+# Optional: Specific functions can be imported if necessary.
+# from .preprocessing import clean_data
+# from .visualization import plot_logs
+# from .clustering import apply_kmeans
